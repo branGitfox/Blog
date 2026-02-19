c2f5d0a14e7caa5db4b794ac6f9a20b5d8f89fd9
BranGitfox <vixfgit@gmail.com>
2026-02-19 16:11:24 +0300

    feat: create_commnet success fully done

--- app/repository/commentRepository.py
+++ app/repository/commentRepository.py
@@ -1,3 +1,5 @@
+import datetime
+
 from sqlalchemy.orm import Session
 from app import schemas, models
 
@@ -5,7 +7,7 @@ from app import schemas, models
 class CommentRepository:
     @staticmethod
     def create(db: Session, request: schemas.PostComment, user_id:int):
-        new_comment = models.Comment(content=request.content, user_id=user_id, blog_id=request.blog_id)
+        new_comment = models.Comment(content=request.content, user_id=user_id, blog_id=request.blog_id, created_at=datetime.datetime.now())
         db.add(new_comment)
         db.commit()
         db.refresh(new_comment)
